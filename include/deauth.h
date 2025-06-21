7a60d7ed1224f5890411030e166b69d3c7f66ae3
DingDang <97514372+MeowCata@users.noreply.github.com>
2025-06-21 08:52:16 +0800

    Add files via upload

--- include/deauth.h
+++ include/deauth.h
@@ -5,9 +5,10 @@
 
 void start_deauth(int wifi_number, int attack_type, uint16_t reason);
 void stop_deauth();
-void deauth_all();  // Declare new function
+void deauth_all();
 
 extern int eliminated_stations;
 extern int deauth_type;
+extern int deauth_iterations;
 
 #endif
